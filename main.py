f9cf6e9bfbfea57cdbbd30cbebca6e7679d776be
Kareem James <kareemdjames@gmail.com>
2022-04-17 20:31:53 -0400

    added some error checking and user friendly messages

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ full_list = []
 
 def main_request(baseurl, endpoint, page_no):
     page_no = f'?page={page_no}'
-    r = requests.get(baseurl + endpoint + page_no)
+    try:
+        r = requests.get(baseurl + endpoint + page_no)
+    except BaseException:
+        print("Failed to retrieve data")
+
     return r.json()
 
 
@@ -20,6 +24,7 @@ def parse_json(response):
     character_list = []
     for item in response['results']:
         character = {
+            'id': item['id'],
             'name': item['name'],
             'no_of_episodes': len(item['episode'])
         }
@@ -28,13 +33,21 @@ def parse_json(response):
 
     return character_list
 
+try:
+    print('Attempting to fetch data')
+    data = main_request(baseurl, endpoint, 1)
+    for page in range(1, get_pages(data) + 1):
+        full_list.extend(parse_json(main_request(baseurl, endpoint, page)))
+    print('Successfully fetched the data')
+    try:
+        print("Attempting to create csv")
+        df = pd.DataFrame(full_list)
+        df.to_csv('characterlist.csv', index=False)
+        print('Successfully created csv')
+    except BaseException:
+        print('Failed to create csv')
+except BaseException:
+    print('Unable to fetch data')
 
-data = main_request(baseurl, endpoint, 1)
-for page in range(1, get_pages(data) + 1):
-    #print(page)
-    full_list.extend(parse_json(main_request(baseurl, endpoint, page)))
 
-df = pd.DataFrame(full_list)
-
-print(df.head(), df.tail())
 

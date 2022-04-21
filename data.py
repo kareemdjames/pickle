baca07a2f7334781f25b8ee74625d26c20da428c
Kareem James <kareemdjames@gmail.com>
2022-04-21 14:36:22 -0400

    adding functions to get image url and then download image locally

--- data.py
+++ data.py
@@ -1,8 +1,7 @@
+import shutil
+
 import requests
 import pandas as pd
-
-
-
 
 baseurl = 'https://rickandmortyapi.com/api/'
 endpoint = 'character'
@@ -28,7 +27,7 @@ def parse_json(response):
     for item in response['results']:
         character = {
             'id': item['id'],
-            'name': item['name'],
+            'char_name': item['name'],
             'no_of_episodes': len(item['episode']),
             'image': item['image']
         }
@@ -37,24 +36,52 @@ def parse_json(response):
 
     return character_list
 
-def start():
-    try:
-        print('Attempting to fetch data')
-        data = main_request(baseurl, endpoint, 1)
-        for page in range(1, get_pages(data) + 1):
-            full_list.extend(parse_json(main_request(baseurl, endpoint, page)))
-        print('Successfully fetched the data')
-        try:
-            print("Attempting to create csv")
-            df = pd.DataFrame(full_list)
-            df.to_csv('characterlist.csv', index=False)
-            print('Successfully created csv')
-        except BaseException:
-            print('Failed to create csv')
-    except BaseException:
-        print('Unable to fetch data')
 
-    return full_list
+# def start():
+#     try:
+#         print('Attempting to fetch data')
+#         data = main_request(baseurl, endpoint, 1)
+#         for page in range(1, get_pages(data) + 1):
+#             full_list.extend(parse_json(main_request(baseurl, endpoint, page)))
+#         print('Successfully fetched the data')
+#         try:
+#             print("Attempting to create csv")
+#             df = pd.DataFrame(full_list)
+#             df.to_csv('characterlist.csv', index=False)
+#             print('Successfully created csv')
+#         except BaseException:
+#             print('Failed to create csv')
+#     except BaseException:
+#         print('Unable to fetch data')
+#
+#     return full_list
 
 
+def get_image_urls():
+    df = pd.read_csv('characterlist.csv', nrows=10)
+    print(df.to_string())
+    row_list = []
+    for i, rows in df.iterrows():
+        url_and_name_list = [rows.char_name, rows.image]
+        row_list.append(url_and_name_list)
+    print(row_list)
+    return row_list
 
+
+def download_images(list_of_images):
+    for row in list_of_images:
+        name = row[0]
+        image_url = row[1]
+        file_name = name + '.jpeg'
+        res = requests.get(image_url, stream=True)
+
+        if res.status_code == 200:
+            with open(file_name, 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded: ', file_name)
+        else:
+            print('Image Couldn\'t be retrieved')
+
+
+download_images(get_image_urls())
+# get_image_urls()

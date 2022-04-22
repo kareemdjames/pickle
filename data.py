5a5e1724bdfea9343288e6eb93a7107cded68f2d
Kareem James <kareemdjames@gmail.com>
2022-04-22 09:36:28 -0400

    added logic to download from bucket, tested successfully

--- data.py
+++ data.py
@@ -44,6 +44,33 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     )
 
 
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your GCS object
+    # source_blob_name = "storage-object-name"
+
+    # The path to which the file should be downloaded
+    # destination_file_name = "local/path/to/file"
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
+
+
 def main_request(baseurl, endpoint, page_no):
     page_no = f'?page={page_no}'
     try:
@@ -121,3 +148,4 @@ def download_images(list_of_images):
 # download_images(get_image_urls())
 # get_image_urls()
 #upload_blob('pickle_image_bucket', 'Rick Sanchez.jpeg', 'Rick Sanchez')
+download_blob('pickle_image_bucket', 'Rick Sanchez', 'pickle rick')
